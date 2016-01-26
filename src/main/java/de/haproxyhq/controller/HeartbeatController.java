fdfcd36fe8b7a635899d95d5e514c49fb0c442d7
Jonas Depoix <jdepoix@seibert-media.net>
2016-01-26 14:04:22 +0100

    code clean up

--- src/main/java/de/haproxyhq/controller/HeartbeatController.java
+++ src/main/java/de/haproxyhq/controller/HeartbeatController.java
@@ -1,8 +1,6 @@
 package de.haproxyhq.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
